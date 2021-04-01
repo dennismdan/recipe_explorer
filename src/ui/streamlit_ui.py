f888d818000d56cb03993172a1458ffc113f2345
jdnunnali <jdnunnali@gmail.com>
2021-03-31 21:28:48 -0400

    Updates to streamlit ui

--- src/ui/streamlit_ui.py
+++ src/ui/streamlit_ui.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
 import time
@@ -8,7 +9,8 @@ import numpy as np
 #from SPARQLWrapper import SPARQLWrapper, JSON
 from streamlit_agraph import agraph, Node, Edge, TripleStore, Config
 #from pyvis.network import Network
-
+from IPython.core.display import display, HTML
+from ..backend.get_cluster import ClusterData
 
 def app():
 
@@ -18,7 +20,7 @@ def app():
   middle, rsidebar = st.beta_columns([3, 1])
   sidebar.title("Choose meal type and ingredients")
   userIngredients = sidebar.text_input("Input ingredients that should be included in returned recipes:")
-  userInput = sidebar.text_input("Input recipe to use as cluster seed: ")
+  userRecipesNum = sidebar.selectbox("Select number of recipes to return: ", options=[10,20,30,40,50], index=0)
 
   # create empty lists for holding nodes and edges for network
   #net = Network() # place holder for transitioning to pyvis
@@ -26,18 +28,19 @@ def app():
   edges = []
   nodeSize = 500
 
-  # Bring in the initial data
-  tmp = pd.read_csv('./cluster_test.csv') # Here is where we would access the data from the backend
-  print('length before filter: ', len(tmp))
-
   # filter data for only recipes that contain the user inputs
   if userIngredients != "":
       ingredients = userIngredients.split(',')
       ingredients = [i.strip() for i in ingredients]
-      contains = [tmp["RecipeIngredientParts"].str.contains(i) for i in ingredients]
-      #print(ingredients)
-      tmp = tmp[np.all(contains, axis=0)]
-      print('length of filtered: ', len(tmp))
+      # Bring in the initial data
+      #tmp = pd.read_csv(r'C:\Users\jdnun\OneDrive\Documents\GT\Spring2021\Project\code\cse6242_project-main\src\cluster_test.csv') # Here is where we would access the data from the backend
+      tmp = ClusterData(stringInput= ingredients, topNrRecipes=userRecipesNum)
+      print('length before filter: ', len(tmp))
+      #contains = [tmp["RecipeIngredientParts"].str.contains(i) for i in ingredients]
+      # print(ingredients)
+      #tmp = tmp[np.all(contains, axis=0)]
+      #print('length of filtered: ', len(tmp))
+
 
 
   if len(tmp) != 0:
@@ -55,15 +58,16 @@ def app():
       #                                target=int(tmp["c1"]["nodes"].iloc[j]["RecipeId"]), type="CURVE_SMOOTH")])
       for i in range(0, len(tmp["nodes"])):
           nodes.extend([Node(id=int(tmp.iloc[i]['RecipeId']), label=tmp.iloc[i]["Name"],
-                             size=nodeSize)])
-          #net.add_node(n_id=int(tmp.iloc[i]['RecipeId']), value=nodeSize, label=tmp.iloc[i]["Name"]) # place holder for transitioning to pyvis
+                            size=nodeSize)])
+          #net.add_node(int(tmp.iloc[i]['RecipeId']), value=nodeSize, label=tmp.iloc[i]["Name"]) # place holder for transitioning to pyvis
+      for i in range(0, len(tmp["nodes"])):
           for j in range(0, len(tmp["nodes"])):
               if tmp.iloc[i]['RecipeId'] != tmp.iloc[j]['RecipeId'] and int(tmp.iloc[i]["Ingredient Difference"]) == int(tmp.iloc[j]["Ingredient Difference"]) \
                       and [int(tmp.iloc[j]['RecipeId']),int(tmp.iloc[i]['RecipeId'])] not in edgeList:
                   edgeList.append([int(tmp.iloc[i]['RecipeId']),int(tmp.iloc[j]['RecipeId'])])
                   edges.extend([Edge(source=int(tmp.iloc[i]['RecipeId']), label=int(tmp.iloc[j]["Ingredient Difference"]),
                                      target=int(tmp.iloc[j]["RecipeId"]), type="CURVE_SMOOTH")])
-                  #net.add_edge(int(tmp.iloc[i]['RecipeId']),int(tmp.iloc[j]["RecipeId"]),title=int(tmp.iloc[j]["Ingredient Difference"])) # # place holder for transitioning to pyvis
+                  #net.add_edge(int(tmp.iloc[j]["RecipeId"]),int(tmp.iloc[i]['RecipeId']),title=int(tmp.iloc[j]["Ingredient Difference"])) # # place holder for transitioning to pyvis
 
 
       # diffList = [str(i) for i in set(tmp["c1"]["nodes"]["Ingredient Difference"])]
@@ -71,7 +75,9 @@ def app():
       # diffList.sort(reverse=True)
       # display_category = sidebar.selectbox("Ingredient difference: ",index=0, options = diffList) # could add more stuff here later on or add other endpoints in the sidebar.
       # if display_category == "all":
+
       viewEdges = edges
+
       # else:
       #     viewEdges = [edge for edge in edges if edge.label == display_category]
       #mealType = sidebar.selectbox("Meal Type: ", index=0, options = ["Breakfast", "Lunch", "Dinner"]) # Just a place holder could be 'soup', 'salad', or 'italian', 'indian', etc
@@ -92,13 +98,18 @@ def app():
           return_value = agraph(nodes=nodes,
                                  edges=viewEdges,
                                  config=config)
-            #net.show('testgraph.html') # place holder for transitioning to pyvis
+          # HtmlFile = open('C:/Users/jdnun/AppData/Local/Programs/Python/Python37/testgraph.html', 'r', encoding='utf-8')
+          # source_code = HtmlFile.read()
+          # components.html(source_code, height = 900, width = 1000)
+          #net.show('testgraph.html') # place holder for transitioning to pyvis
+          #display(HTML('net.html'))
 
 
 
 
 
-  st.text("Showing recipes based on the following ingredients: {}".format(userIngredients))
+
+  st.text("Showing recipes based on the following user inputs: {}".format(userIngredients))
 
 if __name__=='__main__':
     app()
